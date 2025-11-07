b555ce56e98ed9561a4039b4c233023ad23496ec
access <access@gmail.com>
2025-11-07 10:22:15 +0530

    feat: add Plasma component and integrate into hero section

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -4,6 +4,7 @@ import ColorBends from "../ColorBends";
 import { motion } from "motion/react";
 import Navigation from "../ui/navigation";
 import CustomButton, { ImageButton } from "../ui/custom-button";
+import Plasma from "../Plasma";
 
 const Hero = () => {
     return (
@@ -11,8 +12,13 @@ const Hero = () => {
         <div className="min-h-screen w-full bg-black relative">
             {/* Silk Background */}
             <div className="absolute inset-0 z-0">
-                <ColorBends
-                    colors={["#092F52"]}
+                <Plasma
+                    color="#DBB25A"
+                    speed={0.6}
+                    direction="forward"
+                    scale={1.1}
+                    opacity={0.8}
+                    mouseInteractive={true}
                 />
             </div>
 
@@ -30,21 +36,21 @@ const Hero = () => {
                         className="text-center max-w-[1000px] mx-auto "
                     >
                         <h1 className="heading-style  max-w-[971px] mx-auto">
-                        Weaving Financial 
-                        
-                       
-                        Legacies.
+                            Weaving Financial
+
+
+                            Legacies.
                         </h1>
                         <p className="text-description max-w-[669px] mx-auto py-[30px]">
-                        Finvastra partners with NBFCs, fintechs, and institutions to structure debt, unlock capital, and build sustainable financial ecosystems. Together, we transform growth into legacy.
+                            Finvastra partners with NBFCs, fintechs, and institutions to structure debt, unlock capital, and build sustainable financial ecosystems. Together, we transform growth into legacy.
                         </p>
 
                         {/* Industry Tags */}
                         <div className="flex justify-center gap-[10px]">
-                        <CustomButton name="Explore Solutions" onClick={() => {}} />
-                        <CustomButton name="Partner With Us" onClick={() => {}} />
-                        <ImageButton image="/icons/arrow.svg" onClick={() => {}} />
-                            </div>
+                            <CustomButton name="Explore Solutions" onClick={() => { }} />
+                            <CustomButton name="Partner With Us" onClick={() => { }} />
+                            <ImageButton image="/icons/arrow.svg" onClick={() => { }} />
+                        </div>
 
                     </motion.div>
                 </div>
